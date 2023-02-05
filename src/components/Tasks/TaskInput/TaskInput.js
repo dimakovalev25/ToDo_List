50af9fafe9bff86e70a3c5f84da7f14b9616060f
dimakovalev25 <d.nozdryn@gmail.com>
2023-02-06 01:21:26 +0300

    styled component

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -1,14 +1,49 @@
 import React, {useState} from "react";
+import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
 import "./TaskInput.css";
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #c8e1e4;
+    border-color: #00358b;
+  }
+
+  &.invalid input {
+    border-color: orangered;
+    background: #ee8c6d;
+  }
+
+  &.invalid label {
+    color: orangered;
+  }`
+
 
 const TaskInput = (props) => {
     const [inputText, setInputText] = useState("");
     const [isInputValid, setIsInputValid] = useState(true);
 
     const taskInputChangeHandler = (event) => {
-        if(event.target.value.trim().length > 0) {
+        if (event.target.value.trim().length > 0) {
             setIsInputValid(true)
         }
         setInputText(event.target.value);
@@ -26,12 +61,11 @@ const TaskInput = (props) => {
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <div className="form-control">
-                <label
-                style={{color: !isInputValid ? 'red' : 'black' }}
-                >Задачи</label>
-                <input style={{borderColor: !isInputValid ? 'red' : 'black'}} value={inputText} type="text" onChange={taskInputChangeHandler}/>
-            </div>
+            <FormControl className={!isInputValid && 'invalid'}>
+                      <label>Задачи</label>
+                    <input value={inputText} type="text" onChange={taskInputChangeHandler}/>
+            </FormControl>
+
             <Button type="submit">Добавить Задачу</Button>
         </form>
     );
